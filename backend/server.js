9d2ed249742b3b468ad5fbe1d3392153be29bfea
dltmdan92 <dltmdan92@naver.com>
2021-10-03 11:31:42 +0900

    데이터베이스 테이블 명 입력 잘못한 부분 수정

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // 테이블 생성하기
-db.pool.query(`CREATE TABLE LISTS (
+db.pool.query(`CREATE TABLE lists (
     id INTEGER AUTO_INCREMENT,
     value TEXT,
     PRIMARY KEY (id)
